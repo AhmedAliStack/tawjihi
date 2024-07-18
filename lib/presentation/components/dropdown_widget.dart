f61726f69f3563b9821b4d5f4e4d2824b8f0369d
MuhamedFaraag <max.farag2020@gmail.com>
2024-07-18 21:36:42 +0300

    fix icons

--- lib/presentation/components/dropdown_widget.dart
+++ lib/presentation/components/dropdown_widget.dart
@@ -123,73 +123,73 @@ class DropdownWidget extends StatelessWidget {
       onChanged: onItemSelected,
       validator: validator,
 
-      // icon: Icon(
-      //   Icons.keyboard_arrow_down,
-      //   color: iconEnabledColor ?? mauveColor,
-      // ),
-      // iconSize: 24.w,
-      // iconEnabledColor: primaryColor,
-      // iconDisabledColor: Colors.grey,
-      // buttonHeight: buttonHeight.h,
-      // buttonWidth: buttonWidth,
-      // buttonPadding:
-      //     buttonPadding ?? const EdgeInsetsDirectional.only(start: 14, end: 14),
-      // buttonDecoration: BoxDecoration(
-      //   borderRadius: BorderRadius.circular(8),
-      //   border: Border.all(color: borderMainColor),
-      //   color: Colors.white,
-      // ),
-      // buttonElevation: buttonElevation ? 1 : 0,
-      // itemHeight: 40,
-      // itemPadding: const EdgeInsets.only(left: 16, right: 16),
-      // dropdownMaxHeight: 200,
-      // dropdownWidth: 0.9.sw,
-      // dropdownPadding: null,
-      // dropdownDecoration: BoxDecoration(
-      //   borderRadius: BorderRadius.circular(buttonBorderRaduis ?? 8.r),
-      //   color: Colors.white,
-      // ),
-      // dropdownElevation: 1,
-      // scrollbarRadius: const Radius.circular(40),
-      // scrollbarThickness: 6,
-      // scrollbarAlwaysShow: true,
-      // offset: const Offset(0, 0),
-      // searchController: textEditingController,
-      // searchInnerWidgetHeight: 50,
-      // searchInnerWidget: enableSearch
-      //     ? Padding(
-      //         padding: const EdgeInsets.only(
-      //           top: 8,
-      //           bottom: 4,
-      //           right: 8,
-      //           left: 8,
-      //         ),
-      //         child: TextFormField(
-      //           controller: textEditingController,
-      //           decoration: InputDecoration(
-      //             isDense: true,
-      //             contentPadding: const EdgeInsets.symmetric(
-      //               horizontal: 10,
-      //               vertical: 8,
-      //             ),
-      //             hintText: 'بحث',
-      //             hintStyle: const TextStyle(fontSize: 12),
-      //             border: OutlineInputBorder(
-      //               borderRadius: BorderRadius.circular(8),
-      //             ),
-      //           ),
-      //         ),
-      //       )
-      //     : const SizedBox(),
-      // searchMatchFn: (item, searchValue) {
-      //   return (item.value.toString().contains(searchValue));
-      // },
-      // //  This to clear the search value when you close the menu
-      // onMenuStateChange: (isOpen) {
-      //   if (!isOpen) {
-      //     textEditingController?.clear();
-      //   }
-      // },
+      icon: Icon(
+        Icons.keyboard_arrow_down,
+        color: iconEnabledColor ?? mauveColor,
+      ),
+      iconSize: 24.w,
+      iconEnabledColor: primaryColor,
+      iconDisabledColor: Colors.grey,
+      buttonHeight: buttonHeight.h,
+      buttonWidth: buttonWidth,
+      buttonPadding:
+          buttonPadding ?? const EdgeInsetsDirectional.only(start: 14, end: 14),
+      buttonDecoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: borderMainColor),
+        color: Colors.white,
+      ),
+      buttonElevation: buttonElevation ? 1 : 0,
+      itemHeight: 40,
+      itemPadding: const EdgeInsets.only(left: 16, right: 16),
+      dropdownMaxHeight: 200,
+      dropdownWidth: 0.9.sw,
+      dropdownPadding: null,
+      dropdownDecoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(buttonBorderRaduis ?? 8.r),
+        color: Colors.white,
+      ),
+      dropdownElevation: 1,
+      scrollbarRadius: const Radius.circular(40),
+      scrollbarThickness: 6,
+      scrollbarAlwaysShow: true,
+      offset: const Offset(0, 0),
+      searchController: textEditingController,
+      searchInnerWidgetHeight: 50,
+      searchInnerWidget: enableSearch
+          ? Padding(
+              padding: const EdgeInsets.only(
+                top: 8,
+                bottom: 4,
+                right: 8,
+                left: 8,
+              ),
+              child: TextFormField(
+                controller: textEditingController,
+                decoration: InputDecoration(
+                  isDense: true,
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 8,
+                  ),
+                  hintText: 'بحث',
+                  hintStyle: const TextStyle(fontSize: 12),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            )
+          : const SizedBox(),
+      searchMatchFn: (item, searchValue) {
+        return (item.value.toString().contains(searchValue));
+      },
+      //  This to clear the search value when you close the menu
+      onMenuStateChange: (isOpen) {
+        if (!isOpen) {
+          textEditingController?.clear();
+        }
+      },
     );
   }
 }
